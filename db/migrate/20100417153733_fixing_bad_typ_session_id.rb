0796145a775f047c42093a9a79194574d44916aa
Antonio Garrote <antoniogarrote@gmail.com>
2010-04-17 17:43:55 +0200

    WIP

--- db/migrate/20100417153733_fixing_bad_typ_session_id.rb
+++
@@ -1,9 +0,0 @@
-class FixingBadTypSessionId < ActiveRecord::Migration
-  def self.up
-    add_column :interventions, :session_id, :integer
-  end
-
-  def self.down
-    add_column :interventions, :session_id, :integer
-  end
-end
