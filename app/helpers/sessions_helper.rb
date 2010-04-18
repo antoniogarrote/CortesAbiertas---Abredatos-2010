ab978ba059057ffc6cebab2302cfe5b65d8f424f
Antonio Garrote <antoniogarrote@gmail.com>
2010-04-18 14:45:46 +0200

    max results words API

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+  def data_json
+    js = StringIO.new
+    js << "SessionDataCortesAbiertas = {"
+    Session.all.each_with_index do |s,i|
+      if i != 0
+        js << " , "
+      end
+      js << "session_#{s.id}: "
+      js << s.all_top_words.map{ |w| w.to_hash }.to_json
+    end
+    js << "}"
+
+    return js.string
+  end
 end
