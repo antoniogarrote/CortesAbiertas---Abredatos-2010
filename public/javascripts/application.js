6f68a1b50ac9d1bf5bddb6118fa1630285b86b1f
Antonio Garrote <antoniogarrote@gmail.com>
2010-04-18 12:36:50 +0200

    sessions view WIP

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+CortesAbiertas = {
+
+    show_points_of_day: function(id) {
+        var div = 'session_summary_' + id;
+        var link = 'session_summary_pod_link' + id;
+        var old = jQuery("#session_summary_pod_link_4").text()
+        if(old === "mostrar") {
+            jQuery("#session_summary_pod_link_4").text("ocultar")
+        } else {
+            jQuery("#session_summary_pod_link_4").text("mostrar")
+        }
+        $(div).toggle();
+    },
+
+    hello: function() {
+        alert("hola");
+    }
+}
